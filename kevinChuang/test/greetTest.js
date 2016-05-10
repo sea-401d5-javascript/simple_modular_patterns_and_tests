a5db47d0b1073d9aac77c3d5751b3a3b96426d05
Kevin Chuang <kcxinkun@gmail.com>
2016-05-10 01:06:08 -0700

    implement exports.greetings, usable in command line, and is tested for both

---
+++ kevinChuang/test/greetTest.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+const greet = require('../greet');
+
+describe('greeting test', ()=>{
+  it('should greet the name that is input',()=>{
+    var name;
+    expect(greet.greetings('Kevin')).to.eql('Hello Kevin');
+  });
+});
+
+describe('command line greeting', ()=> {
+  it('should greet the name of argument passed in the command line', ()=> {
+    process.argv = ['node','greet.js','Kevin'];
+    expect(greet.greetings(process.argv[2])).to.eql('Hello Kevin');
+  });
+});
